be0089f8fa930f5483e96955a1289363f6952f20
yelicherlabhargav <82765416+yelicherlabhargav@users.noreply.github.com>
2021-05-03 11:44:22 +0530

    initial

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import flask,requests,json 
 import jinja2
 from authlib.flask.client import OAuth
+from flask import Flask, redirect, url_for, render_template, request, flash
 
 app = flask.Flask(__name__)
 
